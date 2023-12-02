2612c70d2666c0494fc383c5f8ea446e5aabcf35
Haseeb Qureshi <haseebqureshi@Haseebs-MacBook-Air.local>
2023-12-01 23:41:40 -0800

    Complete day 1

--- day01.rb
+++ day01.rb
@@ -2,14 +2,25 @@ DATA = File.readlines('data01.txt').map(&:chomp)
 
 # Part 1
 
-def first_num(s)
-  s.chars.find { |c| c[/\d/] }
+sum = DATA.sum do |s|
+  (s[/\d/] + s.reverse[/\d/]).to_i
 end
 
-calibration_values = DATA.map do |s|
-  (first_num(s) + first_num(s.reverse)).to_i
-end
-
-puts calibration_values.sum
+puts sum
 
 # Part 2
+
+WORDS = %w(zero one two three four five six seven eight nine)
+NUMS = (0..9).map(&:to_s)
+MATCH_REGEX = (WORDS + NUMS).join('|')
+
+def recalibrated_num(s)
+  WORDS.index(s) || NUMS.index(s)
+end
+
+recalibrated_sum = DATA.sum do |s|
+  matches = s.scan(/(?=(#{MATCH_REGEX}))/).flatten
+  10 * recalibrated_num(matches[0]) + recalibrated_num(matches[-1])
+end
+
+puts recalibrated_sum
