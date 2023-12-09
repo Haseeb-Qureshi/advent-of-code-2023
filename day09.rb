842301388c9825846acc96259a6e6489a3fe3a99
Haseeb Qureshi <haseebqureshi@Haseebs-MacBook-Air.local>
2023-12-08 23:28:43 -0800

    Complete day 9 part 2

--- day09.rb
+++ day09.rb
@@ -1,13 +1,27 @@
 LINES = File.readlines('data09.txt').map { |l| l.chomp.split.map(&:to_i) }
 
 def line_to_final_value(line)
-  diffs = [line.each_cons(2).map { |a, b| b - a}]
+  diffs = [line, line.each_cons(2).map { |a, b| b - a}]
   until diffs.last.all?(&:zero?)
     diffs << diffs.last.each_cons(2).map { |a, b| b - a }
   end
-  ([line] + diffs).map(&:last).sum
+  diffs.map(&:last).sum
 end
 
 # Part 1
 
-puts LINES.sum { |line| p line_to_final_value(line) }
+puts LINES.sum { |line| line_to_final_value(line) }
+
+# Part 2
+
+def line_to_first_value(line)
+  diffs = [line, line.each_cons(2).map { |a, b| b - a}]
+  until diffs.last.all?(&:zero?)
+    diffs << diffs.last.each_cons(2).map { |a, b| b - a }
+  end
+  diffs.reverse.reduce(0) do |current, diff_array|
+    diff_array.first - current
+  end
+end
+
+puts LINES.sum { |line| line_to_first_value(line) }
